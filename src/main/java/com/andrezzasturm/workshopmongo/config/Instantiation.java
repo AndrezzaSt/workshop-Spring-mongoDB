2129efbe2284f888f27f7616df25a114a05bf4bf
andrezza_sturm <andrezza_sturm@sicredi.com.br>
2021-11-25 12:45:50 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/andrezzasturm/workshopmongo/config/Instantiation.java
+++ src/main/java/com/andrezzasturm/workshopmongo/config/Instantiation.java
@@ -4,6 +4,7 @@ package com.andrezzasturm.workshopmongo.config;
 import com.andrezzasturm.workshopmongo.domain.Post;
 import com.andrezzasturm.workshopmongo.domain.User;
 import com.andrezzasturm.workshopmongo.dto.AuthorDTO;
+import com.andrezzasturm.workshopmongo.dto.CommentDTO;
 import com.andrezzasturm.workshopmongo.repository.PostRepository;
 import com.andrezzasturm.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,11 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
 
         userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
         postReposiroty.saveAll(Arrays.asList(post1, post2));
