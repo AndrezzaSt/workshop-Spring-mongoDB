42b69f5cbfbfe0764e356b2a87d9ad62fa27a07c
andrezza_sturm <andrezza_sturm@sicredi.com.br>
2021-11-25 13:02:01 -0300

    Consulta simples com @ query

--- src/main/java/com/andrezzasturm/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/andrezzasturm/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.andrezzasturm.workshopmongo.repository;
 
 import com.andrezzasturm.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,6 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 
     List<Post> findByTitleContainingIgnoreCase(String text);
-
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
