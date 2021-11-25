61f88d847853bc7139510118b496d0a6ae4245b1
andrezza_sturm <andrezza_sturm@sicredi.com.br>
2021-11-25 12:52:29 -0300

    Consulta simples com query methods

--- src/main/java/com/andrezzasturm/workshopmongo/services/PostService.java
+++ src/main/java/com/andrezzasturm/workshopmongo/services/PostService.java
@@ -4,6 +4,8 @@ import com.andrezzasturm.workshopmongo.domain.Post;
 import com.andrezzasturm.workshopmongo.repository.PostRepository;
 import com.andrezzasturm.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,5 +17,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
